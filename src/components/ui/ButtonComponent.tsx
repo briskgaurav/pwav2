bd4448ac88b7960262dd141f32dd4b2e013c348f
Hitesh Bhardwaj <bhardwajhitesh09@gmail.com>
2026-03-27 15:22:40 +0530

    Update

--- src/components/ui/ButtonComponent.tsx
+++ src/components/ui/ButtonComponent.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function ButtonComponent({ title, onClick, disabled=false, isfixed=true , styleType='solid'}: { title: string, onClick: () => void, disabled?: boolean, isfixed?: boolean, styleType?: 'solid' | 'border'  }) {
   return (
     <button 
-      className={` ${isfixed ? 'fixed bottom-[5vw] left-1/2 -translate-x-1/2' : ''}  w-[90%]  py-4 rounded-full font-medium ${disabled ? 'cursor-not-allowed bg-purple-600 text-white' : styleType === 'solid' ? 'bg-primary text-white' : 'border border-text-primary text-text-primary'}`} 
+      className={` ${isfixed ? 'fixed bottom-[5vw] left-1/2 -translate-x-1/2' : ''}  w-[90%]  py-4 rounded-full font-medium ${disabled ? 'cursor-not-allowed bg-disable-button text-white' : styleType === 'solid' ? 'bg-primary text-white' : 'border border-text-primary text-text-primary'}`} 
       onClick={disabled ? undefined : onClick} 
       disabled={disabled}
     >
