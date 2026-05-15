aa29fea2a3888973a30e19dca7663085a8614a7f
SunilKulhadiya <141410499+SunilKulhadiya@users.noreply.github.com>
2026-05-15 16:08:13 +0530

    Removed unwanted code12

--- src/app/instacard/add-universal-card/uc-scan/page.tsx
+++ src/app/instacard/add-universal-card/uc-scan/page.tsx
@@ -1,3 +1,15 @@
+// import UcScanScreen from '@/components/screens/AddInstacardScreens/UniversalCardScreens/UCScanScreen'
+// import LayoutSheet from '@/components/ui/LayoutSheet'
+// import React from 'react'
+
+// export default function page() {
+//     return (
+//         <LayoutSheet needPadding={false} routeTitle="Scan Your Universal Card">
+//             {/* <UcScanScreen /> */}
+            
+//         </LayoutSheet>
+//     )
+// }
 import UcScanScreen from '@/components/screens/AddInstacardScreens/UniversalCardScreens/UCScanScreen'
 import LayoutSheet from '@/components/ui/LayoutSheet'
 import { useRouter } from 'next/navigation'
