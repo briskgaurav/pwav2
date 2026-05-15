64a2c34aa45713da0301de528fe1687945c09cca
SunilKulhadiya <141410499+SunilKulhadiya@users.noreply.github.com>
2026-05-15 16:00:08 +0530

    giftcard3

--- src/app/instacard/add-universal-card/uc-scan/page.tsx
+++ src/app/instacard/add-universal-card/uc-scan/page.tsx
@@ -1,46 +1,14 @@
-<<<<<<< HEAD
 import UcScanScreen from '@/components/screens/AddInstacardScreens/UniversalCardScreens/UCScanScreen'
 import LayoutSheet from '@/components/ui/LayoutSheet'
 import { useRouter } from 'next/navigation'
-=======
-// import UcScanScreen from '@/components/screens/AddInstacardScreens/UniversalCardScreens/UCScanScreen'
-// import LayoutSheet from '@/components/ui/LayoutSheet'
-// import React from 'react'
 
-// export default function page() {
-//     return (
-//         <LayoutSheet needPadding={false} routeTitle="Scan Your Universal Card">
-//             {/* <UcScanScreen /> */}
-            
-//         </LayoutSheet>
-//     )
-// }
-
->>>>>>> fbb91b9f305ed80122b7964aeb817f30f627f148
 import React from 'react'
 import { routes } from '@/lib/routes'
 import type { UserUniveralCardSteps } from '@/types/userVerificationSteps'
 
 export default function page() {
-<<<<<<< HEAD
-    const router = useRouter()
-
-    const handleNext = (step: UserUniveralCardSteps) => {
-        if (step === 'registered_email_verification') {
-            router.push(routes.addUniversalVerifyEmail)
-        }
-        // Add other steps if needed
-    }
-
-    return (
-        <LayoutSheet needPadding={false} routeTitle="Scan Your Universal Card">
-            <UcScanScreen handleNext={handleNext} />
-        </LayoutSheet>
-    )
-=======
   return (
     <div>page</div>
   )
->>>>>>> fbb91b9f305ed80122b7964aeb817f30f627f148
 }
 
