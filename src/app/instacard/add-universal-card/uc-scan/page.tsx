7594437aa41df840490b5b8c789a87fa507d4723
SunilKulhadiya <141410499+SunilKulhadiya@users.noreply.github.com>
2026-05-15 16:13:23 +0530

    Removed unwanted code123

--- src/app/instacard/add-universal-card/uc-scan/page.tsx
+++ src/app/instacard/add-universal-card/uc-scan/page.tsx
@@ -10,17 +10,12 @@
 //         </LayoutSheet>
 //     )
 // }
-import UcScanScreen from '@/components/screens/AddInstacardScreens/UniversalCardScreens/UCScanScreen'
-import LayoutSheet from '@/components/ui/LayoutSheet'
-import { useRouter } from 'next/navigation'
 
 import React from 'react'
-import { routes } from '@/lib/routes'
-import type { UserUniveralCardSteps } from '@/types/userVerificationSteps'
 
 export default function page() {
   return (
-    <div>page</div>
+        <div>page</div>
   )
 }
 
